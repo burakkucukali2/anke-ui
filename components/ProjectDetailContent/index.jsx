61f57f1405ae5742e04f493cd58465129cd00797
burakkucukali1 <burakkucukali6@gmail.com>
2023-04-19 15:58:43 +0300

    add project detail component

---
+++ components/ProjectDetailContent/index.jsx
@@ -0,0 +1,131 @@
+import React from "react";
+import { Spinner } from "@/components";
+import Image from "next/image";
+import { splitDateAccordingToMinusSignAndReverse } from "@/utils/helper";
+import { useTranslation } from "next-i18next";
+import { useRouter } from "next/router";
+import styles from "./index.module.css";
+
+function ProjectDetailContent({ projectData, isLoading }) {
+  const { t } = useTranslation("common");
+  const { locale } = useRouter();
+
+  const projectInfoValueAndKey = [
+    {
+      key: "projectOwner",
+      value: projectData?.data?.attributes?.projectOwner,
+    },
+    {
+      key: "projectFeature",
+      value: projectData?.data?.attributes[locale]?.projectFeature,
+    },
+    {
+      key: "structureFeature",
+      value: projectData?.data?.attributes[locale]?.structureFeature,
+    },
+    {
+      key: "moldArea",
+      value: projectData?.data?.attributes?.moldArea,
+    },
+    {
+      key: "location",
+      value: projectData?.data?.attributes?.location,
+    },
+    {
+      key: "startDate",
+      value: splitDateAccordingToMinusSignAndReverse(
+        projectData?.data?.attributes?.startDate ?? ""
+      ),
+    },
+    {
+      key: "endDate",
+      value: splitDateAccordingToMinusSignAndReverse(
+        projectData?.data?.attributes?.endDate
+      ),
+    },
+  ];
+
+  const projectBoxValueAndKey = [
+    {
+      key: "totalArea",
+      value: projectData?.data?.attributes?.totalArea,
+      unit: "㎡",
+    },
+    {
+      key: "ironAmount",
+      value: projectData?.data?.attributes?.ironAmount,
+      unit: "Ton",
+    },
+    {
+      key: "concreteAmount",
+      value: projectData?.data?.attributes?.concreteAmount,
+      unit: "m³",
+    },
+  ];
+
+  const renderProjectInfos = () => {
+    return (
+      <div className={styles["info"]}>
+        {projectInfoValueAndKey.map((item) => (
+          <div key={item.key}>
+            {item.value && (
+              <div key={item.key} className={styles["info-item"]}>
+                <span className={styles["info-item-key"]}>{t(item.key)}:</span>
+                <span className={styles["info-item-value"]}>{item.value}</span>
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  const renderBoxes = () => {
+    return (
+      <div className={styles["box-group"]}>
+        {projectBoxValueAndKey.map((item) => (
+          <div key={item.key}>
+            {item.value && (
+              <div key={item.key} className={styles["box"]}>
+                <div className={styles["box-value"]}>
+                  <div className={styles["box-value-text"]}>{item.value}</div>
+                  <div className={styles["box-value-unit"]}>{item.unit}</div>
+                </div>
+                <div className={styles["box-title"]}>
+                  {t(`common:${item.key}`)}
+                </div>
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  return (
+    <>
+      {isLoading ? (
+        <Spinner isLoading={isLoading} />
+      ) : (
+        <div className={styles["wrapper"]}>
+          <Image
+            src={projectData?.data?.attributes.imgUrl ?? "/turnkey-img.png"}
+            alt={projectData?.data?.attributes[locale].name}
+            width={710}
+            height={400}
+            className={styles["image"]}
+          />
+          <div className={styles["info-section"]}>
+            <div className={styles["title"]}>
+              {projectData?.data?.attributes[locale].name}
+            </div>
+            {renderProjectInfos()}
+            {renderBoxes()}
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default ProjectDetailContent;
--- pages/projects/[projectServices]/[projectServiceType]/[projectId].js
+++ pages/projects/[projectServices]/[projectServiceType]/[projectId].js
@@ -3,10 +3,8 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import DynamicHead from "@/components/DynamicHead";
 import { useTranslation } from "next-i18next";
 import { useRouter } from "next/router";
-import styles from "./index.module.css";
-import { splitDateAccordingToMinusSignAndReverse } from "@/utils/helper";
 import { useEffect, useState } from "react";
-import { Spinner } from "@/components";
+import ProjectDetailContent from "@/components/ProjectDetailContent";
 
 export default function ProjectDetail() {
   const { t } = useTranslation("common");
@@ -27,81 +25,6 @@ export default function ProjectDetail() {
     fetchProjectDetail();
   }, []);
 
-  const projectInfoValueAndKey = [
-    {
-      key: "projectOwner",
-      value: projectData?.data?.attributes.projectOwner,
-    },
-    {
-      key: "projectFeature",
-      value: projectData?.data?.attributes[locale].projectFeature,
-    },
-    {
-      key: "structureFeature",
-      value: projectData?.data?.attributes[locale].structureFeature,
-    },
-    {
-      key: "moldArea",
-      value: projectData?.data?.attributes.moldArea,
-    },
-    {
-      key: "location",
-      value: projectData?.data?.attributes.location,
-    },
-    {
-      key: "startDate",
-      value: splitDateAccordingToMinusSignAndReverse(
-        projectData?.data?.attributes.startDate ?? ""
-      ),
-    },
-    {
-      key: "endDate",
-      value: splitDateAccordingToMinusSignAndReverse(
-        projectData?.data?.attributes.endDate
-      ),
-    },
-  ];
-
-  const projectBoxValueAndKey = [
-    {
-      key: "totalArea",
-      value: projectData?.data?.attributes.totalArea,
-      unit: "㎡",
-    },
-    {
-      key: "ironAmount",
-      value: projectData?.data?.attributes.ironAmount,
-      unit: "Ton",
-    },
-    {
-      key: "concreteAmount",
-      value: projectData?.data?.attributes.concreteAmount,
-      unit: "m³",
-    },
-  ];
-
-  const renderBoxes = () => {
-    return (
-      <div className={styles["box-group"]}>
-        {projectBoxValueAndKey.map((item) => (
-          <div key={item.key}>
-            {item.value && (
-              <div key={item.key} className={styles["box"]}>
-                <div className={styles["box-value"]}>
-                  <div className={styles["box-value-text"]}>{item.value}</div>
-                  <div className={styles["box-value-unit"]}>{item.unit}</div>
-                </div>
-                <div className={styles["box-title"]}>
-                  {t(`common:${item.key}`)}
-                </div>
-              </div>
-            )}
-          </div>
-        ))}
-      </div>
-    );
-  };
-  console.log("test");
   return (
     <div className="layout">
       <DynamicHead
@@ -110,49 +33,15 @@ export default function ProjectDetail() {
         )}`}
         description={projectData?.data?.attributes[locale].name}
       />
-      {isLoading ? (
-        <Spinner isLoading={isLoading} />
-      ) : (
-        <div className={styles["wrapper"]}>
-          <Image
-            src={projectData?.data?.attributes.imgUrl}
-            alt={projectData?.data?.attributes[locale].name}
-            width={710}
-            height={400}
-            className={styles["image"]}
-          />
-          <div className={styles["info-section"]}>
-            <div className={styles["title"]}>
-              {projectData?.data?.attributes[locale].name}
-            </div>
-            <div className={styles["info"]}>
-              {projectInfoValueAndKey.map((item) => (
-                <div key={item.key}>
-                  {item.value && (
-                    <div key={item.key} className={styles["info-item"]}>
-                      <span className={styles["info-item-key"]}>
-                        {t(item.key)}:
-                      </span>
-                      <span className={styles["info-item-value"]}>
-                        {item.value}
-                      </span>
-                    </div>
-                  )}
-                </div>
-              ))}
-            </div>
-            {renderBoxes()}
-          </div>
-        </div>
-      )}
+      <ProjectDetailContent projectData={projectData} isLoading={isLoading} />
     </div>
   );
 }
 
-export async function getServerSideProps({ locale }) {
+export async function getServerSideProps(ctx) {
   return {
     props: {
-      ...(await serverSideTranslations(locale, ["common"])),
+      ...(await serverSideTranslations(ctx.locale, ["common"])),
       // Will be passed to the page component as props
     },
   };

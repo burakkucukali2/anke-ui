99e7b43af86c37f81b69a360879291212c35c29a
burakkucukali2 <burak.kucukali@elyt.net>
2023-05-15 00:57:27 +0300

    Set map style for mobile and change contracting locale

--- components/Map/index.jsx
+++ components/Map/index.jsx
@@ -1,48 +1,58 @@
-import React, {useMemo, useRef, useCallback} from "react";
-import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
+import React, { useMemo, useRef, useCallback } from "react";
+import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
 import styles from "./index.module.css";
 
 const containerStyle = {
-    width: '500px',
-    height: '350px',
-    borderRadius: '10px',
+  width: "100%",
+  height: "100%",
+  borderRadius: "10px",
 };
 
 const DEFAULT_CENTER = { lat: 40.9918996, lng: 28.8342029 };
 
 const GOOGLE_MAPS_API_KEY = "AIzaSyD47xPQo1fX4bD5W1MQybxplxj4rhhiq74";
 function Map() {
-    const mapRef = useRef(null);
+  const mapRef = useRef(null);
 
-    const { isLoaded } = useJsApiLoader({
-        id: 'google-map-script',
-        googleMapsApiKey: GOOGLE_MAPS_API_KEY
-    });
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: GOOGLE_MAPS_API_KEY,
+  });
 
-    const center = useMemo(() => (DEFAULT_CENTER), []);
+  const center = useMemo(() => DEFAULT_CENTER, []);
 
-    const options = useMemo(()=> ({
-        disableDefaultUI: true,
-        clickableIcons: false,
-    })  ,[])
+  const options = useMemo(
+    () => ({
+      disableDefaultUI: true,
+      clickableIcons: false,
+    }),
+    []
+  );
 
-    const onLoad = useCallback((map) => (mapRef.current = map), []);
+  const onLoad = useCallback((map) => (mapRef.current = map), []);
 
-    const handleClickOpenNewTabCenter = () => {
-        window.open(`https://www.google.com/maps/search/?api=1&query=${center.lat},${center.lng}`, "_blank");
-    }
+  const handleClickOpenNewTabCenter = () => {
+    window.open(
+      `https://www.google.com/maps/search/?api=1&query=${center.lat},${center.lng}`,
+      "_blank"
+    );
+  };
 
-    return isLoaded ? (
-        <GoogleMap
-            mapContainerStyle={containerStyle}
-            zoom={10}
-            center={center}
-            options={options}
-            onLoad={onLoad}
-        >
-            <Marker position={center} onClick={handleClickOpenNewTabCenter} />
-        </GoogleMap>
-    ) : <></>;
+  return isLoaded ? (
+    <div className={styles["map-container"]}>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        zoom={10}
+        center={center}
+        options={options}
+        onLoad={onLoad}
+      >
+        <Marker position={center} onClick={handleClickOpenNewTabCenter} />
+      </GoogleMap>
+    </div>
+  ) : (
+    <></>
+  );
 }
 
 export default React.memo(Map);
